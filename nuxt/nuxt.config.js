4c982bb0914930efd792047b7c85855690debfb8
TheDanieLSh <95643914+TheDanieLSh@users.noreply.github.com>
2025-01-02 22:57:35 +0300

    Update nuxt.config.js

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'kmazurova',
+    title: 'Ксения Мазурова',
     htmlAttrs: {
       lang: 'en'
     },
@@ -52,5 +52,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+  router: {
+    base: 'thedanielsh.github.io/kmazurova/'
+  },
+  target: 'static'
 }
